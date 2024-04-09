3e087294e0672cfc92c4f84d4f571edb1031919d
Eduardo Ramos <eduardo.ramos@onesource.pt>
2024-04-09 21:26:49 +0100

    init commit, verification of args from cmd

---
+++ logger/__init__.py
@@ -0,0 +1,63 @@
+import os
+import logging
+
+levels = {
+    'CRITICAL': logging.CRITICAL,
+    'ERROR': logging.ERROR,
+    'WARNING': logging.WARNING,
+    'INFO': logging.INFO,
+    'DEBUG': logging.DEBUG
+}
+
+logger_name = "FOLDER-SYNC"
+
+class Logger:
+    
+    def __init__(self, log_path, debug=False):
+        try:
+            log_level = levels.get("INFO" if not debug else "DEBUG", logging.DEBUG)
+
+            logger = logging.getLogger(logger_name)
+            logger.setLevel(log_level)
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+            handler = logging.StreamHandler()
+            handler.setLevel(log_level)
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
+            if not os.path.isdir(f'./{log_path}'):
+                try:
+                    path = os.getcwd()
+                    os.mkdir(f'{path}/{log_path}')
+                except OSError as e:
+                    print(f"Creation of the directory {log_path} failed {e}")
+
+            handler = logging.FileHandler(f"{log_path}/{logger_name}.log")
+
+            handler.setLevel(log_level)
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
+        except Exception as e:
+            raise RuntimeError(f"Error initializing logger: {str(e)}")
+
+    @staticmethod
+    def debug(msg: str, name=logger_name) -> None:
+        logger = logging.getLogger(name)
+        logger.debug(msg)
+    
+    @staticmethod
+    def info(msg: str, name=logger_name) -> None:
+        logger = logging.getLogger(name)
+        logger.info(msg)
+
+    @staticmethod
+    def warning(msg: str, name=logger_name) -> None:
+        logger = logging.getLogger(name)
+        logger.warning(msg)
+
+    @staticmethod
+    def error(msg: str, name=logger_name) -> None:
+        logger = logging.getLogger(name)
+        logger.error(msg)
---
+++ main.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import argparse
+from logger import Logger
+
+def is_absolute_path(path: str):
+    return path.startswith("/")
+
+
+def main():
+    source_folder : str
+    destination_folder : str
+    timeout : int
+    log_path : str
+    debug = False
+    
+    parser = argparse.ArgumentParser(description="Folder syncer script.")
+
+    # Define command-line arguments
+    parser.add_argument('-s', '--source_folder', required=True ,type=str, help='Source folder from where the file will be synced')
+    parser.add_argument('-d', '--destination_folder', required=True, type=str, help='Destination folder to where the folder will be copied to')
+    parser.add_argument('-t', '--time', required=True, help='Time between sync check')
+    parser.add_argument('-l', '--log_path', required=True, help="Destination folder of logs")
+    parser.add_argument('--debug', default=False, help="Enable debug Logs")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    if args.log_path is not None:
+        log_path = args.log_path
+        print(f'Log path: {args.log_path}')
+    else:
+        print("Log path is required")
+        sys.exit(-1)
+    
+    if args.debug is not None:
+        debug = args.debug
+        print(f'Debug: {args.debug}')
+    else:
+        print("Log path is required")
+        sys.exit(-1)
+    
+    Logger(log_path=log_path, debug=debug)
+
+    # Access the values using args.processes, args.traffic_influence, and args.operation
+    if args.source_folder is not None:
+        source_folder = args.source_folder
+        Logger.debug(f'Source Folder: {args.source_folder}')
+    else:
+        Logger.error("Source folder is required")
+        sys.exit(-1)
+    if len(source_folder) <= 0:
+        Logger.error("Invalid source folder value!")
+        sys.exit(-1)
+    
+    if args.destination_folder is not None:
+        destination_folder = args.destination_folder
+        Logger.debug(f'Destination folder: {args.destination_folder}')
+    else:
+        Logger.error("Destination folder is required")
+        sys.exit(-1)
+    if len(destination_folder) <= 0:
+        Logger.error("Invalid destination folder value!")
+        sys.exit(-1)
+    
+    if args.time is not None:
+        timeout = args.time
+        Logger.debug(f'Timeout: {args.time}')
+    else:
+        Logger.error("Timeout is required")
+        sys.exit(-1)
+    
+    try:
+        if not os.path.isdir(source_folder):
+            raise Exception(f"Source folder ({source_folder}) does not exist")
+        
+        if not os.path.isdir(destination_folder):
+            path_split = destination_folder.split('/')
+            if len(path_split) <= 0:
+                raise Exception("Invalid destination path")
+            path_control = ""
+            if not is_absolute_path(destination_folder):
+                i = 0
+            else:
+                i = 1
+                path_control = "/"
+            path_control = path_split[i]
+            while i < len(path_split):
+                if not os.path.isdir(path_control):
+                    os.mkdir(path_control)
+                i += 1
+                path_control += f"/{path_split[i]}"
+        Logger.info("DONE")
+    except Exception as e:
+        Logger.error(f"Error on main [{e}]")
+        sys.exit(-1)
+    
+if __name__ == "__main__":
+    main()
