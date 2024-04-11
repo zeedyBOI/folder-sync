62deb16e1f08a68166c37897ef23d5883bceeb82
Eduardo Ramos <eduardo.ramos@onesource.pt>
2024-04-11 21:31:42 +0100

    copying of files and folders workin (deletion too)

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import sys
 import time
 import argparse
 from logger import Logger
+import shutil
 
+def filter_list(main_list, filter_list):
+    return [item for item in main_list if item not in filter_list]
+
+# ! Checks if it is a relative or an absolute path
 def is_absolute_path(path: str):
     return path.startswith("/")
 
+# ! Check if source directory exists
+# ! Creates destination directory if it doesnt
 def check_directories(source_folder, destination_folder):
     if not os.path.isdir(source_folder):
         raise Exception(f"Source folder ({source_folder}) does not exist")
@@ -23,7 +30,6 @@ def check_directories(source_folder, destination_folder):
             path_control = "/"
         path_control += path_split[i]
         while True:
-            Logger.debug(f"CHECKING IF DIRECTORY ({path_control}) EXISTS - {os.path.isdir(path_control)}")
             if not os.path.isdir(path_control):
                 os.mkdir(path_control)
             i += 1
@@ -31,6 +37,81 @@ def check_directories(source_folder, destination_folder):
                 break
             path_control += f"/{path_split[i]}"
     Logger.info("Source/Destination folders OK")
+
+# ! Iterates over the items in the folder and checks if is folder or file and acts accordingly
+def check_items_in_folder(source_folder, destination_folder, relative_path):
+    items_source_folder = os.listdir(f"{source_folder}/{relative_path}")
+    if len(relative_path) > 0 and not relative_path.startswith("/"):
+        relative_path = f"/{relative_path}"
+    for item in items_source_folder:
+        path = f"{source_folder}{relative_path}/{item}"
+        Logger.debug(f"SOURCE PATH: {path}")
+        if os.path.isdir(path):
+            check_folder(source_folder, destination_folder, f"{relative_path}/{item}")
+        elif os.path.isfile(path):
+            check_file(source_folder, destination_folder, f"{relative_path}/{item}")
+    # ! After copying the necessary files it deletes the files no longer present on the source
+    items_dest_folder = os.listdir(f"{destination_folder}{relative_path}")
+    delete_items = filter_list(items_dest_folder, items_source_folder)
+    for item in delete_items:
+        path = f"{destination_folder}{relative_path}/{item}"
+        Logger.debug(f"DESTINATION PATH: {path}")
+        if os.path.isdir(path):
+            delete_folder(path)
+        elif os.path.isfile(path):
+            delete_file(path)
+
+def check_folder(source_path, destination_path, relative_folder_path):
+    if not os.path.isdir(f"{destination_path}{relative_folder_path}"):
+        copy_folder(f"{source_path}{relative_folder_path}", f"{destination_path}{relative_folder_path}")
+        return
+    check_items_in_folder(source_path, destination_path, relative_folder_path)
+
+def copy_folder(_from, _to):
+    try:
+        shutil.copytree(_from, _to, symlinks=True)
+        Logger.info(f"Directory copied from {_from} to {_to}")
+    except Exception as e:
+        Logger.error(f"Error copying file from {_from} to {_to} [{e}]")
+
+def delete_folder(folder_path):
+    try:
+        shutil.rmtree(folder_path)
+        Logger.info(f"Directory {folder_path} removed")
+    except Exception as e:
+        Logger.error(f"Error removing folder {folder_path} [{e}]")
+
+# ! Verify if the file needs to be copied
+def check_file(source_path, destination_path, relative_file_path):
+    # ! Ignores '.swp' files
+    if relative_file_path.endswith(".swp"):
+        return
+    source_file_path = source_path + relative_file_path
+    dest_file_path = destination_path + relative_file_path
+    # ! If file does not exist in destination copy it
+    if not os.path.isfile(dest_file_path):
+        copy_file(source_file_path, dest_file_path)
+        return
+    # ! If file is different (size or last_modified_date) copy it
+    source_file_data = os.stat(source_file_path)
+    destination_file_data = os.stat(dest_file_path)
+    if source_file_data.st_size != destination_file_data.st_size or source_file_data.st_mtime > destination_file_data.st_mtime:
+        copy_file(source_file_path, dest_file_path)         
+
+def copy_file(_from, _to):
+    try:
+        shutil.copy(_from, _to)
+        Logger.info(f"File copied from {_from} to {_to}")
+    except shutil.SameFileError as e:
+        Logger.error(f"Tried to copy file to the same path as source [{_from}]")
+
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+        Logger.info(f"File {file_path} removed")
+    except Exception as e:
+        Logger.error(f"Error deleting file [{file_path}]")
+   
 
 def main():
     source_folder : str
@@ -41,14 +122,14 @@ def main():
     
     parser = argparse.ArgumentParser(description="Folder syncer script.")
 
-    # Define command-line arguments
+    # ! Define command-line arguments
     parser.add_argument('-s', '--source_folder', required=True ,type=str, help='Source folder from where the file will be synced')
     parser.add_argument('-d', '--destination_folder', required=True, type=str, help='Destination folder to where the folder will be copied to')
     parser.add_argument('-t', '--time', required=True, type=int, help='Time between sync check (seconds)')
     parser.add_argument('-l', '--log_path', required=True, help="Destination folder of logs")
     parser.add_argument('--debug', action=argparse.BooleanOptionalAction, help="Enable debug Logs")
 
-    # Parse the command-line arguments
+    # ! Parse the command-line arguments
     args = parser.parse_args()
 
     if args.log_path is not None:
@@ -64,7 +145,7 @@ def main():
     
     Logger(log_path=log_path, debug=debug)
 
-    # Access the values using args.processes, args.traffic_influence, and args.operation
+    # ! Access the arg values
     if args.source_folder is not None:
         source_folder = args.source_folder
         Logger.debug(f'Source Folder: {args.source_folder}')
@@ -99,13 +180,9 @@ def main():
         sys.exit(-1)
     
     while True:
-        items_in_folder = os.listdir(source_folder)
-        for item in items_in_folder:
-            # ! Check if '.swp' files are skipped or not
-            if item.endswith(".swp"):
-                continue
-            print(item)
+        check_items_in_folder(source_folder, destination_folder, "")
         time.sleep(timeout)
     
 if __name__ == "__main__":
     main()
+
